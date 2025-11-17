1a2c973cd3db14a2e0c7936350f644b0845f9ed6
starfir <starfir2@163.com>
2025-11-17 23:30:38 +0800

    Delete build directory

--- build/.qt/deploy_InnovateB_728bb932cd.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/Innovate_course_B/InnovateB/build/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/InnovateB-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE D:/Innovate_course_B/InnovateB/build/InnovateB.exe
-    GENERATE_QT_CONF
-)
